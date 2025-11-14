89ad3e78ee03b089703219fe03d03e3c878f6c6a
chandraaja123 <akuntugaschandra@gmail.com>
2025-11-14 11:12:35 +0700

    menambah fungsi untuk menghapus data orangtua

---
+++ hapus_orangtua.php
@@ -0,0 +1,19 @@
+<?php
+// Panggil koneksi
+include "koneksi.php";
+
+// Ambil data kd_ortu dari URL
+$kd = $_GET['kd_ortu'];
+
+// Query hapus data berdasarkan kd_ortu
+$sql = "delete from tb_orangtua where kd_ortu='$kd'";
+$hasil = mysqli_query($konek, $sql);
+
+
+// Cek hasil eksekusi query
+if(!$hasil){
+    echo "Hapus Data Gagal";
+} else {
+    header("location:index_orangtua.php");
+}
+?>
